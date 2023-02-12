c52b8cd32c619d9edc6d956ddbafdd0d7fecdcd7
Admin <Admin@DESKTOP-DBPOA13>
2023-02-12 13:45:03 +0530

    First Commit.......

---
+++ Downloads/test/gitDemo/src/Demo/Git1.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class Git1 {
+
+	public static void main(String[] args) {
+		
+		int i = 10;
+		
+		if(i>0)
+			System.out.println("Number is positive");
+		else
+			System.out.println("Number is Negative");
+	}
+}
